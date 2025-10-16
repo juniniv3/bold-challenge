8ae064a0a9e756a593af4556cd6a122aea831334
Yisus Florez <juniniprogramador@outlook.com>
2025-10-16 14:49:42 -0500

    feat: first table layout

--- src/app/transactions/transactions-dashboard.ts
+++ src/app/transactions/transactions-dashboard.ts
@@ -1,15 +1,17 @@
 import { Component, inject } from '@angular/core';
 import { TransactionsApi } from './services/transactions-api';
-import { AsyncPipe, JsonPipe } from '@angular/common';
+import { CommonModule, AsyncPipe } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { MoneyValue } from "../shared/design-system/atoms/money-value/money-value";
 
 @Component({
   selector: 'app-transactions',
-  imports: [AsyncPipe, JsonPipe, MoneyValue],
+  imports: [CommonModule, FormsModule, AsyncPipe, MoneyValue],
   templateUrl: './transactions-dashboard.html',
   styleUrl: './transactions-dashboard.scss'
 })
 export class TransactionsDashboard {
   transactionsApi = inject(TransactionsApi);
   transactions$ = this.transactionsApi.getTransactions();
+  filterText = '';
 }
